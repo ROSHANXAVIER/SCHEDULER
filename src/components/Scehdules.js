ede5b336536d660643a89a8c6b8467d93e36f45b
Roshan <rxgody@gmail.com>
2022-09-21 08:50:56 +0530

    delete on work

--- src/components/Scehdules.js
+++ src/components/Scehdules.js
@@ -1,14 +1,83 @@
 import React from 'react'
 import axios from 'axios'
 import { useEffect,useState } from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './Schedules.css'
+import Card from 'react-bootstrap/Card';
+import Accordion from 'react-bootstrap/Accordion';
 
 function Scehdules() {
+  const [today,setToday]=useState([]);
+  const [up,setUp]=useState([]);
+  
   useEffect(()=>{
-    axios.get('http://localhost:8001/scheduleup').then(res=>{console.log(res.data)});
-    axios.get('http://localhost:8001/scheduletoday').then(res=>{console.log(res.data)});
+    getApi();
   },[])
+  async function getApi(){
+    await axios.get('http://localhost:8001/scheduleup').then(res=>{setUp((res.data))});
+    await axios.get('http://localhost:8001/scheduletoday').then(res=>{setToday((res.data))});
+  };
+  async function handleClick(val){
+    const dat={val}
+    await axios.post('http://localhost:8001/scheduledelete',dat).then(res=>{console.log(res.data)});
+    getApi();
+  }
+  
   return (
-    <div>Scehdules</div>
+    <Container fluid >
+      <Row>
+        <Col className='col1' border="light"><div>
+          <h1 className='head'>TODAYS</h1>
+          <div>
+          {
+        today.map((item)=>
+        <div className='head'>
+          <Card border="light">
+      <Card.Header className='cardheader'>
+      <Accordion defaultActiveKey="0" flush>
+      <Accordion.Item >
+        <Accordion.Header><Card.Title>{item.title}</Card.Title></Accordion.Header>
+        <Accordion.Body>
+          {item.body}
+        </Accordion.Body>
+        <button type="button" class="btn btn-outline-dark btn-sm" value={item.title} onClick={()=>{handleClick(item.title)}}>DELETE</button>
+        <button type="button" class="btn btn-outline-dark btn-sm ">UPDATE</button>
+      </Accordion.Item>  
+    </Accordion></Card.Header>
+    </Card>
+
+       </div>)
+      }
+          </div>
+          </div></Col>
+        <Col className='col1'><div>
+          <h1 className='head'>UPCOMING </h1>
+          <div>
+          {
+        up.map((item)=>
+        <div className='head'>
+          <Card border="light">
+      <Card.Header className='cardheader'>
+      <Accordion defaultActiveKey="0" flush>
+      <Accordion.Item >
+        <Accordion.Header><Card.Title>{item.title}</Card.Title></Accordion.Header>
+        <Accordion.Body>
+          {item.body}
+        </Accordion.Body>
+        <button type="button" class="btn btn-outline-dark btn-sm" value={item.title} onClick={()=>{handleClick(item.title)}}>DELETE</button>
+        <button type="button" class="btn btn-outline-dark btn-sm ">UPDATE</button>
+      </Accordion.Item>  
+    </Accordion></Card.Header>
+    </Card>
+
+       </div>)
+      }
+          </div>
+          </div></Col>
+      </Row>
+    </Container>  
   )
 }
 
---
+++ src/components/map.js
@@ -0,0 +1,12 @@
+return (
+    <div>
+      {
+        up.map((item)=>
+        <div>
+        <div>{item.title}
+       </div>
+       <div>{item.body}</div></div>)
+
+      }
+    </div>
+  )
