e26c154d55e008bff17f946d7e2369eef6479ca6
Roshan <rxgody@gmail.com>
2022-10-27 13:00:34 +0530

    update

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -34,9 +34,9 @@ function UncontrolledExample() {
     src="https://images.unsplash.com/photo-1544377137-b01b843568a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80"
     alt="First slide"
   />
-  <Carousel.Caption>
+  <Carousel.Caption  >
     <h3 className='dcolr'>PERSONAL SCHEDULER</h3>
-    <p className='dcolr'>Schedule your life . A personal project build out of MERN stack , bootstrap.</p>
+    <p className='dcolrs'>Schedule your life . A personal project build out of MERN stack , bootstrap.</p>
   </Carousel.Caption>
 </Carousel.Item>}
       {!isDark && <Carousel.Item>
@@ -62,7 +62,7 @@ function UncontrolledExample() {
         />
 
         <Carousel.Caption>
-          <h3 className='dcolr'>Contact Creator</h3>
+          <h3 className='dcolr2'>Contact Creator</h3>
           <a href='/home'><FaLinkedin className='colr' onClick={()=>{window.open("https://www.linkedin.com/in/roshan-xavier-1ab097214/")}}/></a>
           <a href='/home'><SiGmail className='colr' onClick={()=>{window.open("mailto:rxgody@gmail.com")}}/></a>
           <a href='/home'><FaGithub className='colr' onClick={()=>{window.open("https://github.com/ROSHANXAVIER")}}/></a>
