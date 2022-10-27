0802a6247d4971a0e2e30c0fffba1922653aa60f
Roshan <rxgody@gmail.com>
2022-10-27 12:45:31 +0530

    update

--- src/components/contexts/theme.js
+++ src/components/contexts/theme.js
@@ -6,15 +6,9 @@ export const ThemeContext = createContext();
 export const ThemeProvider = ({ children }) => {
   const [isDark, setIsDark] = useState(true);
   const toggleTheme = () => {
-    localStorage.setItem("isDark", JSON.stringify(!isDark));
     setIsDark(!isDark);
   };
  
-
-  useEffect(() => {
-    const isDark = localStorage.getItem("isDark") === "true";
-    setIsDark(isDark);
-  }, []);
 
   return (
     <ThemeContext.Provider value={[{ isDark }, toggleTheme]}>
